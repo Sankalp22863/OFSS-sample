eff61cd75b21d336c70ad29ee48f97e4734b771f
Sankalp22863 <naiksankalp1@gmail.com>
2022-08-24 15:03:59 +0530

    Added the Java code.

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println("How you doing??");
+		System.out.println("Joey doesn't share food.");
+		System.out.println("Monica Chill!!! Go get a beer.");
+		System.out.println("Monica - I Don't need a beer.");
+		System.out.println("Who said it was for you??");
+
+	}
+
+}
